43374a9be4d56dc28e58cb1ea693754b44042483
KrutosVIP <52695559+KrutosVIP@users.noreply.github.com>
2025-06-13 17:52:23 +0200

    a53x: init
    Big thanks to @KrutosVIP. He patched libs (and probably more things) to make AOSP boot on a53x.
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>
    Signed-off-by: ksawlii <ksawery.blaszczak@proton.me>

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),a53x)
+
+$(call add-radio-file-sha1-checked,radio/fld.bin_SM-A536B,b73c9bb657f669bef3bed87fb53a888f5428c359)
+$(call add-radio-file-sha1-checked,radio/harx.bin_SM-A536B,b620ee44bb49f981c2d91412291429a81a96361e)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-A536B,7ed360aae3f34d4d866d7df1cf111a3ad29c27ec)
+$(call add-radio-file-sha1-checked,radio/ldfw.img_SM-A536B,f2742623e477c9d8cd7a3d79adce50fbb376b9af)
+$(call add-radio-file-sha1-checked,radio/models,d801a4b996ef929738c17ec14e9e415e59894d06)
+$(call add-radio-file-sha1-checked,radio/modem.bin_SM-A536B,087eee6b5f867a055e74c09deb6b21d474989358)
+$(call add-radio-file-sha1-checked,radio/modem_debug.bin_SM-A536B,dd0abe785a5864756e2411e0b8d6bc26bc6d89fe)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-A536B,dcdad65976997822ef7a7f9332737dd845b10a03)
+$(call add-radio-file-sha1-checked,radio/tzar.img_SM-A536B,62ccb635ff1e97339c8321135f279636057d9140)
+$(call add-radio-file-sha1-checked,radio/tzsw.img_SM-A536B,d22dbeb4d5c88356bac7a54b17b087eba57916ff)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-A536B,f93fe85a169172039a7d36b1fef22cbdfecc2516)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-A536B,16c6520d2148162150186fbc31bcea49e26c5a5a)
+$(call add-radio-file-sha1-checked,radio/version_SM-A536B,a98c26153c046041184e662aabe97909bebb4c11)
+
+endif
---
+++ a53x-vendor.mk
@@ -0,0 +1,81 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/samsung/a53x
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/a53x/proprietary/vendor/etc/floating_feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/floating_feature.xml \
+    vendor/samsung/a53x/proprietary/vendor/etc/init/cass.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/cass.rc \
+    vendor/samsung/a53x/proprietary/vendor/etc/init/init.vendor.rilcommon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.vendor.rilcommon.rc \
+    vendor/samsung/a53x/proprietary/vendor/etc/init/vaultkeeper_common.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vaultkeeper_common.rc \
+    vendor/samsung/a53x/proprietary/vendor/etc/init/vendor.samsung.rilchip.slsi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.samsung.rilchip.slsi.rc \
+    vendor/samsung/a53x/proprietary/vendor/etc/plmn_delta.bin:$(TARGET_COPY_OUT_VENDOR)/etc/plmn_delta.bin \
+    vendor/samsung/a53x/proprietary/vendor/etc/plmn_delta_chn.bin:$(TARGET_COPY_OUT_VENDOR)/etc/plmn_delta_chn.bin \
+    vendor/samsung/a53x/proprietary/vendor/etc/plmn_delta_hktw.bin:$(TARGET_COPY_OUT_VENDOR)/etc/plmn_delta_hktw.bin \
+    vendor/samsung/a53x/proprietary/vendor/etc/plmn_se13.bin:$(TARGET_COPY_OUT_VENDOR)/etc/plmn_se13.bin \
+    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_bt.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_bt.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/calliope_sram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-000000010081:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000010081 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-000000020081:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000020081 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-000000534b4d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000534b4d \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-000048444350:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000048444350 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-0000534b504d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0000534b504d \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-0050524f4341:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0050524f4341 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-0053545354ab:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0053545354ab \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-00575644524d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-00575644524d \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-42494f535542:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-42494f535542 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-46494e474502:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-46494e474502 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4662436b6d52:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4662436b6d52 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-474154454b45:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-474154454b45 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4b45594d5354:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4b45594d5354 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4d5053545549:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4d5053545549 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4d704e434954:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4d704e434954 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4d70536b566e:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4d70536b566e \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4d7073534d43:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4d7073534d43 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-4d7073617574:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4d7073617574 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-505256544545:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-505256544545 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-5345435f4652:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-5345435f4652 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-53454d655345:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-53454d655345 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-54412d48444d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-54412d48444d \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-54496473706c:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-54496473706c \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-544974684c6c:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-544974684c6c \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-564c544b5052:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-564c544b5052 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-656e676d6f64:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-656e676d6f64 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-657365636f6d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-657365636f6d \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-6b6e78677564:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6b6e78677564 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-6d706f667376:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6d706f667376 \
+    vendor/samsung/a53x/proprietary/vendor/tee/00000000-0000-0000-0000-6d73745f5441:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6d73745f5441 \
+    vendor/samsung/a53x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-494363447256:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-494363447256 \
+    vendor/samsung/a53x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-4d53546d7374:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-4d53546d7374 \
+    vendor/samsung/a53x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-53626f786476:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-53626f786476 \
+    vendor/samsung/a53x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-564c544b4456:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-564c544b4456 \
+    vendor/samsung/a53x/proprietary/vendor/tee/ffffffff-0000-0000-0000-000000000030:$(TARGET_COPY_OUT_VENDOR)/tee/ffffffff-0000-0000-0000-000000000030 \
+    vendor/samsung/a53x/proprietary/vendor/tee/tui/resolution_common/ID00000100:$(TARGET_COPY_OUT_VENDOR)/tee/tui/resolution_common/ID00000100
+
+PRODUCT_PACKAGES += \
+    libsemnativecarrierfeature \
+    libswdap \
+    libSemDataProps \
+    libSemTelephonyProps \
+    libexynoscamera3 \
+    libfloatingfeature \
+    libprotobuf-cpp-full-21.7 \
+    libril_sem \
+    libsec-ril \
+    libstork_shared \
+    libvkmanager_vendor \
+    libvkservice \
+    vendor.samsung.hardware.radio-V1-ndk \
+    vendor.samsung.hardware.radio.bridge-V1-ndk \
+    vendor.samsung.hardware.radio.channel-V1-ndk \
+    vendor.samsung.hardware.radio.data-V1-ndk \
+    vendor.samsung.hardware.radio.messaging-V1-ndk \
+    vendor.samsung.hardware.radio.sim-V1-ndk \
+    vendor.samsung.hardware.radio.exclude.slsi.xml \
+    vendor.samsung.hardware.radio_manifest_2_33.xml \
+    vendor.samsung.hardware.sehradio_manifest_2_33.xml \
+    cass \
+    rild \
+    vaultkeeperd
